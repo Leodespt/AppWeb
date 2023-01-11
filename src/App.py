22bde1e3ad5c4aa720a19ed90c2c3cf64aca18c8
Leoodespt <leodespt@gmail.com>
2023-01-11 12:07:07 +0100

    c

--- src/App.py
+++ src/App.py
@@ -14,7 +14,7 @@ app.index_string = '''<!DOCTYPE html>
 <html>
 <head>
 <title>My Game</title>
-<link rel="manifest" href="./assets/manifest.json" />
+<link rel="manifest" href="../assets/manifest.json" />
 {%metas%}
 {%favicon%}
 {%css%}
@@ -30,7 +30,7 @@ app.index_string = '''<!DOCTYPE html>
     window.addEventListener('load', ()=> {
       navigator
       .serviceWorker
-      .register('./assets/sw01.js')
+      .register('../assets/sw01.js')
       .then(()=>console.log("Ready."))
       .catch(()=>console.log("Err..."));
     });
