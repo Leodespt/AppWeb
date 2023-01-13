e50c21e8df82d4dfa2dcc216ee4e32134d653f89
Leoodespt <leodespt@gmail.com>
2023-01-13 12:16:22 +0100

    c

---
+++ src/Game.py
@@ -0,0 +1,7 @@
+class Game():
+
+    def __init__(self, defi_list, joueur_list,start_time ,n_tours):
+        self.defi_list = defi_list
+        self.joueur_list = joueur_list
+        self.start_time = start_time
+        self.n_tours = n_tours
